a6ef862e996811a224bdf5bfb48e04c95cea6326
linulas <linubran90@gmail.com>
2023-02-02 12:28:07 +0100

    ads style boilerplate

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,104 @@
+<svelte:head>
+	<title>Headless</title>
+	<meta
+		name="description"
+		content="This site aims to demonstrate a static webklient getting data from a headless CMS at buildtime"
+	/>
+</svelte:head>
+
+<div class="container">
+	<slot />
+</div>
+
+<style lang="scss">
+	@import '@fontsource/fira-mono';
+	:global {
+		:root {
+			font-family: $font-body;
+			color: $clr-text;
+			box-sizing: border-box;
+			scroll-behavior: smooth;
+			background-color: $clr-bg-1;
+			@include text-sm;
+			@media screen and (min-width: $media-sm) {
+				@include text-base;
+			}
+		}
+		body {
+			min-height: 100vh;
+			margin: 0;
+			contain: paint;
+		}
+
+		a {
+			color: $clr-link;
+			text-decoration: underline;
+
+			&:hover {
+				color: $clr-purple;
+				text-decoration: none;
+			}
+		}
+
+		h1 {
+			@include text-2xl;
+			margin-top: 0;
+			margin-bottom: 1.25rem;
+			@media screen and (min-width: $media-sm) {
+				@include text-3xl;
+				margin-bottom: 1.5rem;
+			}
+		}
+
+		h2 {
+			@include text-xl;
+			margin: $m-xl;
+			@media screen and (min-width: $media-sm) {
+				@include text-2xl;
+				margin: $m-2xl;
+			}
+		}
+
+		h3 {
+			@include text-lg;
+			margin: $m-lg;
+			@media screen and (min-width: $media-sm) {
+				@include text-xl;
+				margin: $m-xl;
+			}
+		}
+
+		h4 {
+			@include text-md;
+			margin: $m-md;
+			@media screen and (min-width: $media-sm) {
+				@include text-lg;
+				margin: $m-lg;
+			}
+		}
+
+		p {
+			@include text-base;
+			margin: $m-base;
+			@media screen and (min-width: $media-sm) {
+				@include text-md;
+				margin: $m-md;
+			}
+		}
+		.container {
+			width: calc(100% - 2rem);
+			max-width: $page-width;
+			margin: 0 auto;
+		}
+
+		.preamble {
+			@include text-md;
+
+			@media screen and (min-width: $media-sm) {
+				font-size: 1.25rem;
+				line-height: 1.813rem;
+				letter-spacing: -0.01em;
+			}
+		}
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p class="preamble">Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h2>This is a h2</h2>
+<p>This is some text</p>
+<h3>This is a h3</h3>
+<p>This is some text</p>
+<h4>This is a h4</h4>
+<p>This is some text</p>
