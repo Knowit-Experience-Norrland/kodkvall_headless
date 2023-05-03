adb98287582073f6b4859653828b58000dcd6053
Alexander <alexander.volkers@knowit.se>
2023-05-03 19:06:37 +0200

    quick fix

--- src/graphql/query.ts
+++ src/graphql/query.ts
@@ -4,16 +4,62 @@ import { gql } from 'graphql-request';
 export const API_URL =
 	'https://api-eu-central-1-shared-euc1-02.hygraph.com/v2/cldmuilqw1ji601tcbw7sbcmh/master';
 
-const IMAGE_FRAGMENT = gql``;
-
-const TEXT_FRAGMENT = gql``;
-
+const IMAGE_FRAGMENT = gql`
+	fragment Image on Image {
+		id
+		image {
+			url
+			width
+			size
+			mimeType
+			height
+			fileName
+		}
+	}
+`;
+const TEXT_FRAGMENT = gql`
+	fragment Text on Text {
+		heading
+		id
+		stage
+		text {
+			html
+			markdown
+			raw
+			text
+		}
+	}
+`;
 const CONTENT_FRAGMENT = gql`
+	fragment Content on PagecontentUnion {
+		__typename
+		... on Text {
+			...Text
+		}
+		... on Image {
+			...Image
+		}
+	}
 	${IMAGE_FRAGMENT}
 	${TEXT_FRAGMENT}
 `;
 
 export const PAGE_QUERY = gql`
+	query Page($where: PageWhereUniqueInput!) {
+		page(where: $where) {
+			id
+			slug
+			title
+			subtitle
+			hero {
+				id
+				url
+			}
+			content {
+				...Content
+			}
+		}
+	}
 	${CONTENT_FRAGMENT}
 `;
 
--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -2,11 +2,22 @@ export const prerender = true;
 
 import { request } from 'graphql-request';
 import type { PageServerLoad } from './$types';
+import { API_URL, PAGE_QUERY, type PageQuery, type PageQueryVariables } from '../graphql';
 
 export const load = (async () => {
 	// TODO: request page query
 
+	const {page} = await request<PageQuery, PageQueryVariables>({
+		url: API_URL,
+		document: PAGE_QUERY,
+		variables: {
+			where: {
+				slug: 'home'
+			}
+		}
+	});
+
 	return {
-		page: null
+		page
 	};
 }) satisfies PageServerLoad;
